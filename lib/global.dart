6af53a2ebf3cb0ccedbb9dbcd62a080ea11576c6
bashayeralhumaidi <bashayer.alhumaidi@gmail.com>
2026-03-11 12:04:58 +0400

    Add secure storage and API base URL configuration

--- lib/global.dart
+++ lib/global.dart
@@ -1,1 +1,10 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+const String apiBase = String.fromEnvironment(
+	'API_URL',
+	defaultValue: 'https://confirmaapplication-bxfba9gybnhyfvcy.westeurope-01.azurewebsites.net',
+);
+
+final FlutterSecureStorage secureStorage = FlutterSecureStorage();
+
 String globalUserName = "";
